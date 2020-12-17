10663247955d53a804762289736f213c2756f7b4
Adroaldo <hygino@msn.com>
2020-12-17 18:33:02 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/hygino/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.hygino.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hygino.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
