44abb548e58e2bde10ab9a990b9fdc7c973fa4a6
Adroaldo <hygino@msn.com>
2020-12-17 10:19:23 -0300

    Atualização de usuário com PUT

--- src/main/java/com/hygino/workshopmongo/services/UserService.java
+++ src/main/java/com/hygino/workshopmongo/services/UserService.java
@@ -11,34 +11,47 @@ import com.hygino.workshopmongo.dto.UserDTO;
 import com.hygino.workshopmongo.repository.UserRepository;
 import com.hygino.workshopmongo.services.exception.ObjectNotFoundException;
 
-@Service 
+@Service
 public class UserService {
-	
+
 	@Autowired
-	//o spring procura o repositório do objeto com essa anotação
-	//injeta automático a dependêcia
+	// o spring procura o repositório do objeto com essa anotação
+	// injeta automático a dependêcia
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public User  findById(String id) {
+
+	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
 
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName() , objDto.getEmail());
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		// o id não muda
+		
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
