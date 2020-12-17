f4e761e1c9862519fb081b0fa6aac91970480d70
Adroaldo <hygino@msn.com>
2020-12-17 08:59:41 -0300

    Obtendo um usuário por id

--- src/main/java/com/hygino/workshopmongo/services/UserService.java
+++ src/main/java/com/hygino/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.hygino.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hygino.workshopmongo.domain.User;
 import com.hygino.workshopmongo.repository.UserRepository;
+import com.hygino.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service 
 public class UserService {
@@ -18,5 +20,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User  findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
