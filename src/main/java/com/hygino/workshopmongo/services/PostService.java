80733618a970c2ebb5f25166dcc77d515b1d4887
Adroaldo <hygino@msn.com>
2020-12-18 08:15:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/hygino/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.hygino.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hygino.workshopmongo.domain.Post;
+import com.hygino.workshopmongo.repository.PostRepository;
+import com.hygino.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	// o spring procura o repositório do objeto com essa anotação
+	// injeta automático a dependêcia
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
